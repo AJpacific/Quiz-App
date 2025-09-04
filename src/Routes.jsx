43b2c7a2191a953f7128e06640fb3258471e69d7
aj <aj4ashutoshjha@gmail.com>
2025-09-05 02:29:10 +0530

    Update

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import { HashRouter, Routes as RouterRoutes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import ErrorBoundary from "./components/ErrorBoundary";
 import NotFound from "./pages/NotFound";
@@ -9,19 +9,19 @@ import QuizInterface from './pages/quiz-interface';
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ErrorBoundary>
-      <ScrollToTop />
-      <RouterRoutes>
-        {/* Define your route here */}
-        <Route path="/" element={<QuizSetup />} />
-        <Route path="/results-summary" element={<ResultsSummary />} />
-        <Route path="/quiz-setup" element={<QuizSetup />} />
-        <Route path="/quiz-interface" element={<QuizInterface />} />
-        <Route path="*" element={<NotFound />} />
-      </RouterRoutes>
+        <ScrollToTop />
+        <RouterRoutes>
+          {/* Define your route here */}
+          <Route path="/" element={<QuizSetup />} />
+          <Route path="/results-summary" element={<ResultsSummary />} />
+          <Route path="/quiz-setup" element={<QuizSetup />} />
+          <Route path="/quiz-interface" element={<QuizInterface />} />
+          <Route path="*" element={<NotFound />} />
+        </RouterRoutes>
       </ErrorBoundary>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
